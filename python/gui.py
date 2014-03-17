227338f3efd6081a025a3cb09e234e86127c9298
Guillaume Lahaie <glahaie@gmail.com>
2014-03-16 20:22:00 -0400

    Fenêtre principale ouvre

--- python/gui.py
+++ python/gui.py
@@ -1,14 +1,42 @@
 # -*- coding: utf-8 -*-
 
-try:
-    from PySide import QtCore
-    from PySide import QtGui
-except:
-    from PyQt4.QtCore import pyqtSlot as Slot
-    from PyQt4 import QtCore
-    from PyQt4 import QtGui
 
-class GUI(QMainWindow):
-    def __init__(self):
-        QMainWindow.__init__(self)
-        pass
+"""Fenêtre principale de l'application, avec le menu"""
+
+import sys
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+class GUI(QApplication):
+    def __init__(self, args):
+        """Création de la fenêtre"""
+        QApplication.__init__(self,args)
+        self.main = QMainWindow()
+        self.main.setWindowTitle("Gestion des vols")
+        self.main.resize(1000,600)
+
+        #widget central : QTextEdit
+        self.edit = QTextEdit("editor", self.main)
+        self.edit.setFocus()
+        self.main.setCentralWidget(self.edit)
+
+        # ajout facile d'éléments dans la barre de menu
+        # NOTE : crée la barre s'il n'existe pas
+        self.__creerMenu()
+
+        self.main.show()
+        self.connect(self,SIGNAL("lastWindowClosed()"),self,SLOT("quit()"))
+        self.exec_()
+
+    def __creerMenu(self):
+        self.main.menuBar().addAction("Lire un fichier vols")
+        self.main.menuBar().addAction("Enregistrer les vols")
+        self.main.menuBar().addAction("Quitter")
+
+        # changement du message de status
+        # NOTE : crée la zone de status si inexistante
+        self.main.statusBar().showMessage("Gestion des vols", 10000)
+
+
+if __name__ == "__main__" :
+    app=GUI(sys.argv)
